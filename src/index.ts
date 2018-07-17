c8d9b508dea1c0cd05f9f92461ef4c87ffafd608
Felix Gladisch <felix@felixgladisch.de>
2018-07-17 09:49:37 +0200

    fix: use cookie parser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
+import cookieParser = require("cookie-parser");
 import cors = require("cors");
 import express = require("express");
 import { Server } from "http";
-
 import { errorHandlerMiddleware, extractHeaderMiddleware } from "./middlewares";
 import { Adapter, Controller } from "./models";
 
@@ -10,6 +10,7 @@ const port: number = Number(process.env.PORT) || 8080;
 const app: express.Application = express();
 
 app.use(cors({ credentials: true, origin: true }));
+app.use(cookieParser());
 app.use(extractHeaderMiddleware);
 
 export function start(adapter: Adapter): Server {
