e3c4e3653fbf2e3e8ce299ca0c980562af4e40ff
Benjamin Kluck <kluck@sipgate.de>
2019-03-22 10:35:56 +0100

    feat: Optimized error logging

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -2,15 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import { ServerError } from "../models";
 
 export function errorHandlerMiddleware(
-	err: Error | ServerError,
+	error: Error | ServerError,
 	req: Request,
 	res: Response,
 	next: NextFunction
 ): void {
-	if (err instanceof ServerError) {
-		res.status(err.status).send(err.message);
+	console.error(error);
+
+	if (error instanceof ServerError) {
+		res.status(error.status).send(error.message);
 		return;
 	}
 
-	res.status(500).send(err.message);
+	res.status(500).send(error.message);
 }
